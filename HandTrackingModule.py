e0175d7fd8b0f7511959f45768cdaf17c5eb131a
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-19 03:57:07 +0300

    HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -3,24 +3,26 @@ import mediapipe as mp
 import time
 import math
 
+
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def _init_(self, mode=False, maxHands=1,modelComplexity=1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
+        self.modelComplex = modelComplexity
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(static_image_mode=self.mode,
-                                        max_num_hands=self.maxHands,
-                                        min_detection_confidence=self.detectionCon,
-                                        min_tracking_confidence=self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplex,
+                                        self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
+
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
@@ -28,43 +30,51 @@ class handDetector():
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
 
+
     def findPosition(self, img, handNo=0, draw=True):
         xList = []
         yList = []
         bbox = []
         self.lmList = []
+
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 xList.append(cx)
                 yList.append(cy)
+                # print(id, cx, cy)
                 self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-            xmin, xmax = min(xList), max(xList)
-            ymin, ymax = min(yList), max(yList)
-            bbox = xmin, ymin, xmax, ymax
+                    xmin, xmax = min(xList), max(xList)
+                    ymin, ymax = min(yList), max(yList)
+                    bbox = xmin, ymin, xmax, ymax
 
-            if draw:
-                cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
-                              (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
+                if draw:
+                    cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+                                (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
 
         return self.lmList, bbox
 
+
     def fingersUp(self):
         fingers = []
+        # Thumb
         if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
+        # 4 Fingers
         for id in range(1, 5):
             if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
         return fingers
+
 
     def findDistance(self, p1, p2, img, draw=True):
         x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
@@ -80,26 +90,28 @@ class handDetector():
         length = math.hypot(x2 - x1, y2 - y1)
         return length, img, [x1, y1, x2, y2, cx, cy]
 
+
 def main():
     pTime = 0
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     detector = handDetector()
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
-        lmList, bbox = detector.findPosition(img)
+        lmList = detector.findPosition(img)
         if len(lmList) != 0:
             print(lmList[4])
 
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
 
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 255), 3)
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 255), 3)
 
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
 
-if __name__ == "__main__":
+
+if __name__ == "_main_":
     main()
--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -2,7 +2,8 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
-
+import math
+import pulsectl
 
 ###########################
 wCam, hCam = 1280, 720
@@ -13,16 +14,60 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
-detector = htm.handDetector()
+detector = htm.handDetector(detectionCon=0.7)
+
+# PulseAudio kontrolünü başlatma
+pulse = pulsectl.Pulse('volume-script')
+
+# Varsayılan sink (çıkış cihazı) alma
+default_sink = pulse.sink_list()[0]
+
+# Mevcut ses seviyesini okuma
+volRange = [0.0, 1.0]  # PulseAudio ses seviyesi 0.0 ile 1.0 arasında değişir
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        if len(lmList) > 8:
+            x1, y1 = lmList[4][1], lmList[4][2]
+            x2, y2 = lmList[8][1], lmList[8][2]
+            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+            length = math.hypot(x2 - x1, y2 - y1)
+
+            vol = np.interp(length, [50, 300], [minVol, maxVol])
+            volPer = np.interp(length, [50, 300], [0, 100])
+            volBar = np.interp(length, [50, 300], [400, 150])
+
+            pulse.volume_set_all_chans(default_sink, vol)
+            print(f'Volume: {volPer}%')
+
+
+            if length < 45:
+                cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+
+
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, f'FPS: {int(fps)}',(40,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),3)
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+    cv2.putText(img, f'Vol: {int(volPer)}%', (40, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
